5b53bc71edf952f80e96ee016f333f4310349f90
Jeferson Oliveira <zuruckyt@gmail.com>
2021-05-16 17:05:45 -0300

    wip

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once(__DIR__ . '../vendor/autoload.php');
+require_once(__DIR__ . '\..\vendor\autoload.php');
+
+use App\Services\MysqlDumperService;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '\..\\');
+$dotenv->load();
+
+$temp_file = MysqlDumperService::dumpDatabase();

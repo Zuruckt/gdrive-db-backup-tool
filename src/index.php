6db6841d4d5174ea3a0c6c0a5b379cdd3af9a4a3
Jeferson Oliveira <zuruckyt@gmail.com>
2021-05-17 19:01:30 -0300

    Add usage of GoogleDriveService

--- src/index.php
+++ src/index.php
@@ -2,9 +2,19 @@
 
 require_once(__DIR__ . '\..\vendor\autoload.php');
 
+use App\Services\GoogleDriveService;
 use App\Services\MysqlDumperService;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '\..\\');
 $dotenv->load();
 
-$temp_file = MysqlDumperService::dumpDatabase();
+$fileName = MysqlDumperService::dumpDatabase();
+$fileContent = file_get_contents('dumps/' . $fileName);
+
+$drive = new GoogleDriveService;
+
+$uploadedFile = $drive->uploadFile($fileName, $fileContent);
+
+echo 'File uploaded succesfully.' . PHP_EOL;
+echo 'Uploaded to: database_backups' . PHP_EOL; 
+echo 'Filename: ' . $uploadedFile->getName() . PHP_EOL;
